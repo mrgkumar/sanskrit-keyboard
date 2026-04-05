cf108ea71453aedfc7a1fe72171d8ef8071681db
mrgkumar <ganeshkumar.mr@gmail.com>
2026-04-05 12:08:30 +0530

    chore: bump version to 20260405.1206

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated during build
-export const BUILD_VERSION = '20260405.1202';
-export const BUILD_TIME = '2026-04-05T06:32:20.924Z';
+export const BUILD_VERSION = '20260405.1206';
+export const BUILD_TIME = '2026-04-05T06:36:22.437Z';
